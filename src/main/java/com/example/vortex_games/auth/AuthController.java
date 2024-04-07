05f3fc67e49568951697629380249ff248d34f94
EduMonti1990 <GatuCapo@gmail.com>
2024-04-07 12:56:08 -0300

    Validacion de registro de usuario doble

--- src/main/java/com/example/vortex_games/auth/AuthController.java
+++ src/main/java/com/example/vortex_games/auth/AuthController.java
@@ -2,9 +2,12 @@ package com.example.vortex_games.auth;
 
 
 import com.example.vortex_games.entity.User;
+import com.example.vortex_games.exception.ExistingProductException;
 import com.example.vortex_games.exception.ResourceNotFoundException;
 import com.example.vortex_games.service.AuthService;
+import com.example.vortex_games.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
@@ -16,7 +19,8 @@ import java.util.Optional;
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
-
+    @Autowired
+    private UserService userService;
     private final AuthService authService;
     @PostMapping(value = "login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request){
@@ -24,7 +28,9 @@ public class AuthController {
     }
 
     @PostMapping(value = "register")
-    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request){
+    public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) throws ExistingProductException {
+        Optional<User> usuarioBuscado = userService.buscarUsuarioPorName(request.username);
+        if(usuarioBuscado.isPresent()) throw new ExistingProductException("Ya existe un usuario con esos datos");
         return ResponseEntity.ok(authService.register(request));
     }
 
